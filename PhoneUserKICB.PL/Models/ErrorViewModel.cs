32fce06dfce9df39c8b111d4ecd99cbdd3965d69
Atai Abdisamat <imankanov2002@gmail.com>
2025-01-19 10:18:46 +0600

    First commit. Created 2 library class project , 1 mvc project. Created gitignore file

---
+++ PhoneUserKICB.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhoneUserKICB.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

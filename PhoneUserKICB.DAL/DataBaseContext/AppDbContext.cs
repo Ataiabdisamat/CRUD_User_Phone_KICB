d5150d541cce93ed35a0c302c54fc06fbd992397
Atai Abdisamat <imankanov2002@gmail.com>
2025-01-19 10:40:34 +0600

    in DAL/ added dbcontext and downloaded 3 packages Microsoft.EntityFrameworkCore/ .Tools / .SQLite

---
+++ PhoneUserKICB.DAL/DataBaseContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneUserKICB.DAL.Entities;
+
+namespace PhoneUserKICB.DAL.DataBaseContext
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
